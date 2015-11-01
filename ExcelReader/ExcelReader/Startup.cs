d2e5637b51039e9544b3c52416fc0563711128ef
nis0420 <nis0420@hotmail.com>
2015-10-31 22:08:24 -0500

    LinqToExcel Reader

---
+++ ExcelReader/ExcelReader/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExcelReader.Startup))]
+namespace ExcelReader
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
